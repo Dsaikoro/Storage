a0acc161c5b772d0ea40089d630f8612892ffe5b
DaisukeNakano <d.saikoro@gmail.com>
2015-12-21 07:15:57 +0900

    edit something

--- hoge3.c
+++ hoge3.c
@@ -1,21 +1,20 @@
 #include <stdio.h>
 #include <math.h>
 
+#define N 100
+
 int main(void)
 {
   float y,ans;
-  float angle = 0;
-  int i,n;
-  float t,dt=0.01;
+  float angle, i, t, dt = 0.01;
   float wavelen = 0.1;
 
-  n = 1000;
-  for(angle = -M_PI/4.0; angle < M_PI/4.0; angle += M_PI/1000.){
+  for(angle = -M_PI/4.; angle < M_PI/4.; angle += M_PI/1000.){
     ans = 0;
-    for(t = 0; t < 2*M_PI ; t += dt){
+    for(t = 0; t < 2.*M_PI ; t += dt){
       y = 0;
-      for(i = 0; i < n; i++){
-	y += 1.0/n *sin(2*M_PI/wavelen * i/n*sin(angle) + t);
+      for(i = 0; i < N; i++){
+	y += 1./N *sin(2.*M_PI/wavelen * i/N*sin(angle) + t);
       }
       ans += y*y*dt;
     }
--- hoge4.c
+++ hoge4.c
@@ -1,27 +1,26 @@
 #include <stdio.h>
 #include <math.h>
 
-#define N 1000
+#define N 100
 
 int main(void)
 {
   float x, y, z, ans, l, lk, dk; 
-  float angle, t, dt , i;
+  float angle, i, t, dt = 0.01;
   float wavelen = 0.1;
 
-  dt = 0.01;
-  x = 10;
+  x = 150.;
 
-  for(angle = -M_PI/4; angle < M_PI/4.0; angle += M_PI/500.){
+  for(angle = -M_PI/4.; angle < M_PI/4.; angle += M_PI/500.){
     ans = 0;
     z = x * tan(angle);
     l = sqrt(x*x + z*z);
-    for(t = 0; t < 2*M_PI; t += dt){
+    for(t = 0; t < 2.*M_PI; t += dt){
       y = 0;
       for(i = 0; i < N; i++){
 	lk = sqrt(x*x + (z+i/N) * (z+i/N));
 	dk = lk - l;
-	y += 1.0/N * sin(2*M_PI/wavelen*dk + t);
+	y += 1./N * sin(2.*M_PI/wavelen*dk + t);
       }
       ans += y*y*dt;
     }
